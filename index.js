a056d94a97bdc8e713cd442ae86648a760f95956
Md Mohibullah <145287917+programmermahi@users.noreply.github.com>
2026-02-03 23:34:36 +0600

    operators in js

---
+++ index.js
@@ -0,0 +1,44 @@
+console.log("Hello from Day 3!");
+
+//let x=4+5
+
+// arithmetic operators
+let x = 10;
+let y = 20;
+let f_name ="Mohibullah";
+let l_name ="Almahin";
+
+console.log("Full Name: " + f_name + " " + l_name);
+console.log("Addition: " + (x + y));
+console.log("Subtraction: " + (x - y));
+console.log("Multiplication: " + (x * y));
+console.log("Division: " + (x / y));
+console.log("Modulus: " + (x % y));
+console.log("Exponentiation: " + (x ** y));
+
+let count=5;
+count++;
+console.log("Incremented Count: " + count);
+count--;
+console.log("Decremented Count: " + count);
+
+
+// assignment operators
+console.log("*** assignment operators ***");
+let a = 10;
+console.log("Initial a: " + a);
+a += 5; // a = a + 5
+console.log("After a += 5: " + a);
+a -= 3;
+console.log("After a -= 3: " + a);
+a *= 2;
+console.log("After a *= 2: " + a);
+a /= 4;
+console.log("After a /= 4: " + a);
+a %= 3;
+console.log("After a %= 3: " + a);
+a **= 2;
+console.log("After a **= 2: " + a);
+
+
+
