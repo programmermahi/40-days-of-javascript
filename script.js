97cbce971aac1f3bf99fc3dccbd75d8ef21e448a
Md Mohibullah <145287917+programmermahi@users.noreply.github.com>
2026-02-03 19:16:21 +0600

    Day 2  Finished
    Day 2

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("Hello from nodejs");
+const personName = "Mohibullah";
+let age = 24;
+let isStudent = true;
+const favoriteLanguage = "JavaScript";
+console.log("Name:", personName);
+console.log("Age:", age);
+console.log("Is Student:", isStudent);
+console.log("Favorite Language:", favoriteLanguage);
+
+const person = {
+  name: "Mohibullah",
+  age: 24
+};
+
+const anotherPerson = person;
+anotherPerson.age = 25;
+
+console.log("Person Age:", person.age); // 25 😮
